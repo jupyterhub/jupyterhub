96765eb681aa4a9d3620cd7650cdb04bed3013d2
MinRK <benjaminrk@gmail.com>
2014-09-12 11:38:39 -0700

    check both src and sys.prefix for data_files
    (catches `pip install`, run from src dir)

--- jupyterhub/_data.py
+++ jupyterhub/_data.py
@@ -2,13 +2,20 @@
 
 def get_data_files():
     """Walk up until we find share/jupyter"""
-    import os
-    path = os.path.abspath(os.path.dirname(__file__))
-    while path != '/':
-        share_jupyter = os.path.join(path, 'share', 'jupyter')
-        if os.path.exists(share_jupyter):
-            return share_jupyter
-        path, _ = os.path.split(path)
+    import sys
+    from os.path import join, abspath, dirname, exists, split
+    path = abspath(dirname(__file__))
+    starting_points = [path]
+    if not path.startswith(sys.prefix):
+        starting_points.append(sys.prefix)
+    for path in starting_points:
+        # walk up, looking for prefix/share/jupyter
+        while path != '/':
+            share_jupyter = join(path, 'share', 'jupyter')
+            if exists(join(share_jupyter, 'static', 'components')):
+                return share_jupyter
+            path, _ = split(path)
+    # didn't find it, give up
     return ''
 
 
