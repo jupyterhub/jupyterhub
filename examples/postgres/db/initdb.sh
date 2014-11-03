cdca10378f2754afba3ff1eb1ab4471ff20e1b6a
Scott Sanderson <scoutoss@gmail.com>
2014-11-03 14:34:14 -0500

    DOC: Fix more typos in warning messages.

--- examples/postgres/db/initdb.sh
+++ examples/postgres/db/initdb.sh
@@ -6,7 +6,7 @@ if [ -z "$JPY_PSQL_PASSWORD" ]; then
     echo "Need to set JPY_PSQL_PASSWORD in Dockerfile or via command line.";
     exit 1;
 elif [ "$JPY_PSQL_PASSWORD" == "arglebargle" ]; then
-    echo "WARNING: Running with password!"
+    echo "WARNING: Running with default password!"
     echo "You are STRONGLY ADVISED to use your own password.";
 fi
 echo "";
