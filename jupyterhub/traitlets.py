d6c6ae2c0f0e7e5ade25ba165c7332518de5d0b8
Min RK <benjaminrk@gmail.com>
2014-11-21 17:44:18 -0800

    add URLPrefix traitlet
    ensures leading, trailing /

---
+++ jupyterhub/traitlets.py
@@ -0,0 +1,14 @@
+"""extra traitlets"""
+# Copyright (c) IPython Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+from IPython.utils.traitlets import Unicode
+
+class URLPrefix(Unicode):
+    def validate(self, obj, value):
+        u = super().validate(obj, value)
+        if not u.startswith('/'):
+            u = '/' + u
+        if not u.endswith('/'):
+            u = u + '/'
+        return u
