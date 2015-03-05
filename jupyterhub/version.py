d871bb3218c7909350ebd2dcbda9edec53b1b350
Min RK <benjaminrk@gmail.com>
2015-03-05 12:04:58 -0800

    simplify version_info
    use '.' as separator

--- jupyterhub/version.py
+++ jupyterhub/version.py
@@ -7,9 +7,7 @@ version_info = (
     0,
     0,
     1,
-    'dev', # comment-out this line for a release
+    'dev',
 )
-__version__ = '.'.join(map(str, version_info[:3]))
 
-if len(version_info) > 3:
-    __version__ = '%s-%s' % (__version__, version_info[3])
+__version__ = '.'.join(map(str, version_info))
