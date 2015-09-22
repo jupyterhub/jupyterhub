e5c1414b6a6d0a54336c3931d74fe83e2e9916a3
Min RK <benjaminrk@gmail.com>
2015-09-22 10:14:11 +0200

    update token app test
    now that admin user isn't added by default

--- jupyterhub/tests/test_app.py
+++ jupyterhub/tests/test_app.py
@@ -3,7 +3,6 @@
 import os
 import re
 import sys
-from getpass import getuser
 from subprocess import check_output
 from tempfile import NamedTemporaryFile, TemporaryDirectory
 
@@ -16,7 +15,9 @@ def test_token_app():
     cmd = [sys.executable, '-m', 'jupyterhub', 'token']
     out = check_output(cmd + ['--help-all']).decode('utf8', 'replace')
     with TemporaryDirectory() as td:
-        out = check_output(cmd + [getuser()], cwd=td).decode('utf8', 'replace').strip()
+        with open(os.path.join(td, 'jupyterhub_config.py'), 'w') as f:
+            f.write("c.Authenticator.admin_users={'user'}")
+        out = check_output(cmd + ['user'], cwd=td).decode('utf8', 'replace').strip()
     assert re.match(r'^[a-z0-9]+$', out)
 
 def test_generate_config():
