96269fac0ffce5de02bf2074976f556830289127
Min RK <benjaminrk@gmail.com>
2016-03-08 10:49:02 +0100

    Call `add_user` more often
    - Ensures add_user is called as part of startup *for all users*.
      This was previously only true for users not already in the db.
    - Normalize usernames in whitelist and admin sets
    - Call add_user on new users logged in when there is no whitelist.

--- jupyterhub/handlers/base.py
+++ jupyterhub/handlers/base.py
@@ -200,6 +200,7 @@ class BaseHandler(RequestHandler):
             self.db.add(u)
             self.db.commit()
             user = self._user_from_orm(u)
+            self.authenticator.add_user(user)
         return user
     
     def clear_login_cookie(self, name=None):
