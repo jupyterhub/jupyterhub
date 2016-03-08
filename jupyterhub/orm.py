74e558dad20e0271e8c0061c71766c858a52f691
Min RK <benjaminrk@gmail.com>
2016-03-08 10:58:18 +0100

    set default pool_recycle if using mysql

--- jupyterhub/orm.py
+++ jupyterhub/orm.py
@@ -378,6 +378,8 @@ def new_session_factory(url="sqlite:///:memory:", reset=False, **kwargs):
     """Create a new session at url"""
     if url.startswith('sqlite'):
         kwargs.setdefault('connect_args', {'check_same_thread': False})
+    elif url.startswith('mysql'):
+        kwargs.setdefault('pool_recycle', 60)
 
     if url.endswith(':memory:'):
         # If we're using an in-memory database, ensure that only one connection
