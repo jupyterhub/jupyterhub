b68569f61c003509f68e30ddc5cd9617ea6f178b
Min RK <benjaminrk@gmail.com>
2016-04-11 16:42:41 +0200

    init alembic
    prepare for database migration

---
+++ jupyterhub/alembic/versions/19c0846f6344_base_revision_for_0_5.py
@@ -0,0 +1,24 @@
+"""base revision for 0.5
+
+Revision ID: 19c0846f6344
+Revises: 
+Create Date: 2016-04-11 16:05:34.873288
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '19c0846f6344'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
