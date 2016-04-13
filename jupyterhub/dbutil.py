86e9a3217c91b94a09deabfff6ac4c85db1aca93
Min RK <benjaminrk@gmail.com>
2016-04-13 16:34:36 +0200

    add jupyterhub upgrade-db entry point
    don't do automatic upgrades (yet)
    
    I'm not sure if auto upgrades are a good idea or not.

---
+++ jupyterhub/dbutil.py
@@ -0,0 +1,82 @@
+"""Database utilities for JupyterHub"""
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+# Based on pgcontents.utils.migrate, used under the Apache license.
+
+from contextlib import contextmanager
+import os
+from subprocess import check_call
+import sys
+from tempfile import TemporaryDirectory
+
+_here = os.path.abspath(os.path.dirname(__file__))
+
+ALEMBIC_INI_TEMPLATE_PATH = os.path.join(_here, 'alembic.ini')
+ALEMBIC_DIR = os.path.join(_here, 'alembic')
+
+
+def write_alembic_ini(alembic_ini='alembic.ini', db_url='sqlite:///jupyterhub.sqlite'):
+    """Write a complete alembic.ini from our template.
+    
+    Parameters
+    ----------
+    
+    alembic_ini: str
+        path to the alembic.ini file that should be written.
+    db_url: str
+        The SQLAlchemy database url, e.g. `sqlite:///jupyterhub.sqlite`.
+    """
+    with open(ALEMBIC_INI_TEMPLATE_PATH) as f:
+        alembic_ini_tpl = f.read()
+    
+    with open(alembic_ini, 'w') as f:
+        f.write(
+            alembic_ini_tpl.format(
+                alembic_dir=ALEMBIC_DIR,
+                db_url=db_url,
+            )
+        )
+    
+
+
+@contextmanager
+def _temp_alembic_ini(db_url):
+    """Context manager for temporary JupyterHub alembic directory
+    
+    Temporarily write an alembic.ini file for use with alembic migration scripts.
+    
+    Context manager yields alembic.ini path.
+    
+    Parameters
+    ----------
+    
+    db_url: str
+        The SQLAlchemy database url, e.g. `sqlite:///jupyterhub.sqlite`.
+    
+    Returns
+    -------
+    
+    alembic_ini: str
+        The path to the temporary alembic.ini that we have created.
+        This file will be cleaned up on exit from the context manager.
+    """
+    with TemporaryDirectory() as td:
+        alembic_ini = os.path.join(td, 'alembic.ini')
+        write_alembic_ini(alembic_ini)
+        yield alembic_ini
+
+
+def upgrade(db_url, revision='head'):
+    """Upgrade the given database to revision.
+    
+    db_url: str
+        The SQLAlchemy database url, e.g. `sqlite:///jupyterhub.sqlite`.
+    revision: str [default: head]
+        The alembic revision to upgrade to.
+    """
+    with _temp_alembic_ini(db_url) as alembic_ini:
+        check_call(
+            ['alembic', '-c', alembic_ini, 'upgrade', revision]
+        )
+
