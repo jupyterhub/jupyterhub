fea683f992e79b757bd534d005ecd742e5b94e23
Min RK <benjaminrk@gmail.com>
2017-02-03 12:51:03 +0100

    redirect root to /hub/login instead of authenticator.login_url
    /hub/login always renders a page,
    whereas `authenticator.login_url` may automatically log the user in via redirects,
    causing logout to appear not to work, as redirects immediately cause login again.

--- jupyterhub/handlers/pages.py
+++ jupyterhub/handlers/pages.py
@@ -52,7 +52,7 @@ class RootHandler(BaseHandler):
                 url = url_path_join(self.hub.server.base_url, 'home')
                 self.log.debug("User is not running: %s", url)
         else:
-            url = self.authenticator.login_url(self.base_url)
+            url = url_path_join(self.hub.server.base_url, 'login')
         self.redirect(url)
 
 
