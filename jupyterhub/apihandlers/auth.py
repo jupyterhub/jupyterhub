bf1dd03df30494420245664dc409a135dc01e783
Min RK <benjaminrk@gmail.com>
2017-08-05 14:45:24 +0200

    handle OAuthAccessTokens with no user
    This shouldn’t happen, raise if it does.
    
    If a token API request is authenticated with no user or service, delete the token because it is invalid and return with 404 because it doesn’t correspond to an existing user.

--- jupyterhub/apihandlers/auth.py
+++ jupyterhub/apihandlers/auth.py
@@ -26,6 +26,11 @@ class TokenAPIHandler(APIHandler):
             model = self.user_model(self.users[orm_token.user])
         elif orm_token.service:
             model = self.service_model(orm_token.service)
+        else:
+            self.log.warning("%s has no user or service. Deleting..." % orm_token)
+            self.db.delete(orm_token)
+            self.db.commit()
+            raise web.HTTPError(404)
         self.write(json.dumps(model))
 
     @gen.coroutine
--- jupyterhub/oauth/store.py
+++ jupyterhub/oauth/store.py
@@ -74,7 +74,10 @@ class AccessTokenStore(HubDBMixin, oauth2.store.AccessTokenStore):
         """
         
         user = self.db.query(orm.User).filter(orm.User.id == access_token.user_id).first()
+        if user is None:
+            raise ValueError("No user for access token: %s" % access_token.user_id)
         orm_access_token = orm.OAuthAccessToken(
+            generated=True,
             client_id=access_token.client_id,
             grant_type=access_token.grant_type,
             expires_at=access_token.expires_at,
