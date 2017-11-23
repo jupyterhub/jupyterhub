04e9e0e6878578f393e0563fe1e59ed4a9901ae0
Gladys Nalvarte <gladys.programs@gmail.com>
2017-11-23 15:14:57 +0100

    test check_version
    add coverage for different cases

--- jupyterhub/tests/test_version.py
+++ jupyterhub/tests/test_version.py
@@ -8,6 +8,11 @@ from .._version import _check_version
 
 @pytest.mark.parametrize('hub_version, singleuser_version, log_level, msg', [
     ('0.8.0', '0.8.0', logging.DEBUG, 'both on version'),
+    ('0.8.0', '0.8.1', logging.DEBUG, ''),
+    ('0.8.0', '0.8.0.dev', logging.DEBUG, ''),
+    ('0.8.0', '0.9.0', logging.WARNING, 'This could cause failure to authenticate'),
+    ('', '0.8.0', logging.WARNING, 'Hub has no version header'),
+    ('0.8.0', '', logging.WARNING, 'Single-user server has no version header'),
 ])
 def test_check_version(hub_version, singleuser_version, log_level, msg, caplog):
     log = logging.getLogger()
@@ -17,4 +22,3 @@ def test_check_version(hub_version, singleuser_version, log_level, msg, caplog):
     record = caplog.records[0]
     assert record.levelno == log_level
     assert msg in record.getMessage()
-
