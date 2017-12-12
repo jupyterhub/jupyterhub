2b15464e12c718cf504b6539bd62da65745f2dc9
Min RK <benjaminrk@gmail.com>
2017-12-12 11:24:32 +0100

    make add_columns conditional on table presence
    in database upgrade.
    
    For multi-version upgrades (e.g. 0.7 -> 0.9)

--- jupyterhub/alembic/versions/1cebaf56856c_session_id.py
+++ jupyterhub/alembic/versions/1cebaf56856c_session_id.py
@@ -12,14 +12,37 @@ down_revision = '3ec6993fe20c'
 branch_labels = None
 depends_on = None
 
+import logging
+logger = logging.getLogger('alembic')
+
 from alembic import op
 import sqlalchemy as sa
 
 tables = ('oauth_access_tokens', 'oauth_codes')
 
+def add_column_if_table_exists(table, column):
+    try:
+        op.add_column(table, column)
+    except (sa.exc.OperationalError, sa.exc.ProgrammingError) as e:
+        # check if it failed because the table doesn't exist
+        # if that's the case, do nothing
+        ctx = op.get_context()
+        try:
+            ctx.execute('SELECT 1 FROM {table} LIMIT 1'.format(table=table))
+        except (sa.exc.OperationalError, sa.exc.ProgrammingError) as e2:
+            if e.args == e2.args:
+                # table doesn't exist, no need to upgrade
+                # because jupyterhub will create it on launch
+                logger.warning("Skipping upgrade of absent table: %s" % e)
+                return
+        # if we got here, adding column failed, but selecting from the table succeeded
+        # it was a real error
+        raise
+
 def upgrade():
+    ctx = op.get_context()
     for table in tables:
-        op.add_column(table, sa.Column('session_id', sa.Unicode(255)))
+        add_column_if_table_exists(table, sa.Column('session_id', sa.Unicode(255)))
 
 
 def downgrade():
