034147f6049511472fcf0a52ccfb9295f0903e7b
Min RK <benjaminrk@gmail.com>
2018-04-14 17:49:13 +0200

    add token lists to token page

--- share/jupyterhub/static/js/token.js
+++ share/jupyterhub/static/js/token.js
@@ -1,12 +1,19 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-require(["jquery", "jhapi"], function ($, JHAPI) {
+require(["jquery", "jhapi", "moment"], function ($, JHAPI, moment) {
     "use strict";
 
     var base_url = window.jhdata.base_url;
     var user = window.jhdata.user;
     var api = new JHAPI(base_url);
+
+    $(".time-col").map(function (i, el) {
+        // convert ISO datestamps to nice momentjs ones
+        el = $(el);
+        let m = moment(new Date(el.text().trim()));
+        el.text(m.isValid() ? m.fromNow() : "Never");
+    });
 
     $("#request-token").click(function () {
         api.request_token({
