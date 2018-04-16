32fe3cf61d57d14dbb514701081d3cade4aaf117
Min RK <benjaminrk@gmail.com>
2018-04-16 10:30:09 +0200

    support adding note to new tokens on token page
    turn token request into a form

--- share/jupyterhub/static/js/token.js
+++ share/jupyterhub/static/js/token.js
@@ -1,26 +1,36 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-require(["jquery", "jhapi", "moment"], function ($, JHAPI, moment) {
-    "use strict";
+require(["jquery", "jhapi", "moment"], function($, JHAPI, moment) {
+  "use strict";
 
-    var base_url = window.jhdata.base_url;
-    var user = window.jhdata.user;
-    var api = new JHAPI(base_url);
+  var base_url = window.jhdata.base_url;
+  var user = window.jhdata.user;
+  var api = new JHAPI(base_url);
 
-    $(".time-col").map(function (i, el) {
-        // convert ISO datestamps to nice momentjs ones
-        el = $(el);
-        let m = moment(new Date(el.text().trim()));
-        el.text(m.isValid() ? m.fromNow() : "Never");
-    });
+  $(".time-col").map(function(i, el) {
+    // convert ISO datestamps to nice momentjs ones
+    el = $(el);
+    let m = moment(new Date(el.text().trim()));
+    el.text(m.isValid() ? m.fromNow() : "Never");
+  });
 
-    $("#request-token").click(function () {
-        api.request_token({
-            success: function (reply) {
-                $("#token-result").text(reply.token);
-                $("#token-area").show();
-            }
-        });
+  $("#request-token-form").submit(function() {
+    var note = $("#token-note").val();
+    if (!note.length) {
+      note = "requested via form page";
+    }
+    api.request_token(
+      user,
+      { note: note },
+      {
+        success: function(reply) {
+          $("#token-result").text(reply.token);
+          $("#token-area").show();
+        },
+      }
+    );
+    return false;
+  });
     });
 });
