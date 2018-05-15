907bbb8e9d70bdfb9a4f3cfa0cf54eea997a1265
Min RK <benjaminrk@gmail.com>
2018-05-15 13:21:02 +0200

    0.9.0b2

--- jupyterhub/_version.py
+++ jupyterhub/_version.py
@@ -7,8 +7,8 @@ version_info = (
     0,
     9,
     0,
-    '',  # release
-    'dev',  # dev
+    'b2',  # release
+    # 'dev',  # dev
 )
 
 # pep 440 version: no dot before beta/rc, but before .dev
