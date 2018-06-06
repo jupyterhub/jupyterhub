ab3e7293a4f629361d96379989e019a6f187a6c3
Min RK <benjaminrk@gmail.com>
2018-06-06 10:53:50 +0200

    disable my server link while stop is pending
    makes it a little harder to request a spawn while stop is pending

--- share/jupyterhub/static/js/home.js
+++ share/jupyterhub/static/js/home.js
@@ -1,19 +1,29 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-require(["jquery", "jhapi"], function ($, JHAPI) {
-    "use strict";
+require(["jquery", "jhapi"], function($, JHAPI) {
+  "use strict";
 
-    var base_url = window.jhdata.base_url;
-    var user = window.jhdata.user;
-    var api = new JHAPI(base_url);
+  var base_url = window.jhdata.base_url;
+  var user = window.jhdata.user;
+  var api = new JHAPI(base_url);
 
-    $("#stop").click(function () {
-        api.stop_server(user, {
-            success: function () {
-                $("#stop").hide();
-            }
-        });
+  $("#stop").click(function() {
+    $("#start")
+      .attr("disabled", true)
+      .attr("title", "Your server is stopping")
+      .click(function() {
+        return false;
+      });
+    api.stop_server(user, {
+      success: function() {
+        $("#start")
+          .text("Start My Server")
+          .attr("title", "Start your server")
+          .attr("disabled", false)
+          .off("click");
+        $("#stop").hide();
+      }
     });
-
+  });
 });
