90cc03b3ecc8151745a6aa511ec44ee2c305c5e8
Min RK <benjaminrk@gmail.com>
2018-06-15 15:39:02 +0200

    back to dev

--- jupyterhub/_version.py
+++ jupyterhub/_version.py
@@ -4,11 +4,11 @@
 # Distributed under the terms of the Modified BSD License.
 
 version_info = (
+    1,
     0,
-    9,
     0,
     "",  # release (b1, rc1)
-    # "dev",  # dev
+    "dev",  # dev
 )
 
 # pep 440 version: no dot before beta/rc, but before .dev
