647dc7af5b8530e1e6a2a484ac39c63241aa6dfa
William Krinsman <williamkrinsman@lbl.gov>
2018-08-01 15:59:23 -0700

    Results of previous Travis builds and commits seem to confirm that the tests are hitting line 994 and therefore failing because of the exception I told them to raise. Assuming that the test actually should pass, I will make one more minor change to see if that was what was causing the test to fail possibly unnecessarily. If not, I will just revert back to the previous control flow which didn't bother to check whether or not named servers were enabled.

--- jupyterhub/handlers/base.py
+++ jupyterhub/handlers/base.py
@@ -988,10 +988,10 @@ class UserSpawnHandler(BaseHandler):
             if server_name is None:
                 spawner = user.spawner
             else:
-                if self.allow_named_servers == True:
+                if self.allow_named_servers:
                     spawner = user.spawners[server_name]
                 else:
-                    raise web.HTTPError(500, "Named servers are not enabled.")
+                    raise web.HTTPError(400, "Named servers are not enabled.")
                 
             # First, check for previous failure.
             if (
