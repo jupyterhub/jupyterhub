0c028c71869ccc5975d8342c89098c49cd912b89
Min RK <benjaminrk@gmail.com>
2019-02-25 13:44:10 +0100

    import parsing of Accept header
    used to determine if a request expects JSON or not

--- jupyterhub/utils.py
+++ jupyterhub/utils.py
@@ -18,6 +18,7 @@ from binascii import b2a_hex
 from datetime import datetime
 from datetime import timezone
 from hmac import compare_digest
+from operator import itemgetter
 
 from async_generator import aclosing
 from async_generator import async_generator
@@ -574,3 +575,78 @@ async def iterate_until(deadline_future, generator):
 def utcnow():
     """Return timezone-aware utcnow"""
     return datetime.now(timezone.utc)
+
+
+def _parse_accept_header(accept):
+    """
+    Parse the Accept header *accept*, returning a list with 3-tuples of
+    [(str(media_type), dict(params), float(q_value)),] ordered by q values.
+    If the accept header includes vendor-specific types like::
+        application/vnd.yourcompany.yourproduct-v1.1+json
+    It will actually convert the vendor and version into parameters and
+    convert the content type into `application/json` so appropriate content
+    negotiation decisions can be made.
+    Default `q` for values that are not specified is 1.0
+
+    From: https://gist.github.com/samuraisam/2714195
+    """
+    result = []
+    for media_range in accept.split(","):
+        parts = media_range.split(";")
+        media_type = parts.pop(0).strip()
+        media_params = []
+        # convert vendor-specific content types into something useful (see
+        # docstring)
+        typ, subtyp = media_type.split('/')
+        # check for a + in the sub-type
+        if '+' in subtyp:
+            # if it exists, determine if the subtype is a vendor-specific type
+            vnd, sep, extra = subtyp.partition('+')
+            if vnd.startswith('vnd'):
+                # and then... if it ends in something like "-v1.1" parse the
+                # version out
+                if '-v' in vnd:
+                    vnd, sep, rest = vnd.rpartition('-v')
+                    if len(rest):
+                        # add the version as a media param
+                        try:
+                            version = media_params.append(('version', float(rest)))
+                        except ValueError:
+                            version = 1.0  # could not be parsed
+                # add the vendor code as a media param
+                media_params.append(('vendor', vnd))
+                # and re-write media_type to something like application/json so
+                # it can be used usefully when looking up emitters
+                media_type = '{}/{}'.format(typ, extra)
+        q = 1.0
+        for part in parts:
+            (key, value) = part.lstrip().split("=", 1)
+            key = key.strip()
+            value = value.strip()
+            if key == "q":
+                q = float(value)
+            else:
+                media_params.append((key, value))
+        result.append((media_type, dict(media_params), q))
+    result.sort(key=itemgetter(2))
+    return result
+
+
+def get_accepted_mimetype(accept_header, choices=None):
+    """Return the preferred mimetype from an Accept header
+
+    If `choices` is given, return the first match,
+    otherwise return the first accepted item
+
+    Return None if choices is given and no match is found,
+    or nothing is specified.
+    """
+    for (mime, params, q) in _parse_accept_header(accept_header):
+        if choices:
+            if mime in choices:
+                return mime
+            else:
+                continue
+        else:
+            return mime
+    return None
