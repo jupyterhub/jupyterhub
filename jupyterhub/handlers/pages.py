11e32588d791efe337c20b784a1c1b866bd88751
Nico Rikken <nico@nicorikken.eu>
2019-07-16 10:17:26 +0200

    chore: use most likely fix for custom username property

--- jupyterhub/handlers/pages.py
+++ jupyterhub/handlers/pages.py
@@ -67,7 +67,7 @@ class HomeHandler(BaseHandler):
 
         html = self.render_template(
             'home.html',
-            user=user.unicode_escaped_url,
+            user=user,
             url=url,
             allow_named_servers=self.allow_named_servers,
             named_server_limit_per_user=self.named_server_limit_per_user,
