7fd3271c9bd1f793a550989e5f4f4dfa0a2a6c01
Min RK <benjaminrk@gmail.com>
2019-09-19 15:16:37 +0200

    rely on app fixture to get configured app
    re-run init_eventlog to ensure event logging is hooked up

--- jupyterhub/tests/test_eventlog.py
+++ jupyterhub/tests/test_eventlog.py
@@ -1,17 +1,20 @@
-"""Tests for Eventlogging in JupyterHub. 
+"""Tests for Eventlogging in JupyterHub.
 
-To test a new schema or event, simply add it to the 
-`valid_events` and `invalid_events` variables below. 
+To test a new schema or event, simply add it to the
+`valid_events` and `invalid_events` variables below.
 
 You *shouldn't* need to write new tests.
 """
 import io
 import json
 import logging
+from unittest import mock
+
 import jsonschema
 import pytest
-from .mocking import MockHub
 from traitlets.config import Config
+
+from .mocking import MockHub
 
 
 # To test new schemas, add them to the `valid_events`
@@ -20,60 +23,57 @@ from traitlets.config import Config
 # To test valid events, add event item with the form:
 # { ( '<schema id>', <version> ) : { <event_data> } }
 valid_events = [
-    ('hub.jupyter.org/server-action', 1, dict(action='start', username='test-username', servername='test-servername')),
+    (
+        'hub.jupyter.org/server-action',
+        1,
+        dict(action='start', username='test-username', servername='test-servername'),
+    )
 ]
 
 # To test invalid events, add event item with the form:
 # { ( '<schema id>', <version> ) : { <event_data> } }
 invalid_events = [
     # Missing required keys
-    ('hub.jupyter.org/server-action', 1, dict(action='start')),
+    ('hub.jupyter.org/server-action', 1, dict(action='start'))
 ]
 
 
 @pytest.fixture
-def get_hub_and_sink():
-    """Get a hub instance with all registered schemas and record an event with it."""
-    # Get a mockhub.
-    hub = MockHub.instance()
-    sink =  io.StringIO()
+def eventlog_sink(app):
+    """Return eventlog and sink objects"""
+    sink = io.StringIO()
     handler = logging.StreamHandler(sink)
+    # Update the EventLog config with handler
+    cfg = Config()
+    cfg.EventLog.handlers = [handler]
 
-    def _get_hub_and_sink(schema):
-        # Update the hub config with handler info.
-        cfg = Config()
-        cfg.EventLog.handlers = [handler]
-        cfg.EventLog.allowed_schemas = [schema]
-
-        # Get hub app.
-        hub.update_config(cfg)
-        hub.init_eventlog()
-
-        # Record an event from the hub.
-        return hub, sink
-
-    yield _get_hub_and_sink
-    
-    # teardown
-    hub.clear_instance()
-    handler.flush()
+    with mock.patch.object(app.config, 'EventLog', cfg.EventLog):
+        # recreate the eventlog object with our config
+        app.init_eventlog()
+        # return the sink from the fixture
+        yield app.eventlog, sink
+    # reset eventlog with original config
+    app.init_eventlog()
 
 
 @pytest.mark.parametrize('schema, version, event', valid_events)
-def test_valid_events(get_hub_and_sink, schema, version, event):
-    hub, sink = get_hub_and_sink(schema)
-    # Record event.
-    hub.eventlog.record_event(schema, version, event)
-    # Inspect consumed event.
+def test_valid_events(eventlog_sink, schema, version, event):
+    eventlog, sink = eventlog_sink
+    eventlog.allowed_schemas = [schema]
+    # Record event
+    eventlog.record_event(schema, version, event)
+    # Inspect consumed event
     output = sink.getvalue()
     data = json.loads(output)
     # Verify event data was recorded
     assert data is not None
 
+
 @pytest.mark.parametrize('schema, version, event', invalid_events)
-def test_invalid_events(get_hub_and_sink, schema, version, event):
-    hub, sink = get_hub_and_sink(schema)
-    
-    # Make sure an error is thrown when bad events are recorded.
+def test_invalid_events(eventlog_sink, schema, version, event):
+    eventlog, sink = eventlog_sink
+    eventlog.allowed_schemas = [schema]
+
+    # Make sure an error is thrown when bad events are recorded
     with pytest.raises(jsonschema.ValidationError):
-        recorded_event = hub.eventlog.record_event(schema, version, event)
+        recorded_event = eventlog.record_event(schema, version, event)
