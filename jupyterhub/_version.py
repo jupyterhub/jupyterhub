861a7c5c5e98dbff4f3f71a4d79dd6c743bea907
Erik Sundell <erik.i.sundell@gmail.com>
2019-12-26 18:20:06 +0100

    back to dev

--- jupyterhub/_version.py
+++ jupyterhub/_version.py
@@ -6,8 +6,8 @@ version_info = (
     1,
     1,
     0,
-    "b1",  # release (b1, rc1, or "" for final or dev)
-    # "dev",  # dev or nothing
+    "b2",  # release (b1, rc1, or "" for final or dev)
+    "dev",  # dev or nothing
 )
 
 # pep 440 version: no dot before beta/rc, but before .dev
