cfcd85a188ba40967a938a45cf85ae7fdad7d994
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-02-28 15:17:16 +0100

    Start named servers by pressing the Enter key

--- share/jupyterhub/static/js/home.js
+++ share/jupyterhub/static/js/home.js
@@ -50,6 +50,17 @@ require(["jquery", "moment", "jhapi", "utils"], function(
     }
   }
 
+  function startServer() {
+    var row = getRow($(this));
+    var serverName = row.find(".new-server-name").val();
+    if (serverName === "") {
+      // ../spawn/user/ causes a 404, ../spawn/user redirects correctly to the default server
+      window.location.href = "./spawn/" + user;
+    } else {
+      window.location.href = "./spawn/" + user + "/" + serverName;
+    }
+  }
+
   function stopServer() {
     var row = getRow($(this));
     var serverName = row.data("server-name");
@@ -100,14 +111,10 @@ require(["jquery", "moment", "jhapi", "utils"], function(
     });
   });
 
-  $(".new-server-btn").click(function() {
-    var row = getRow($(this));
-    var serverName = row.find(".new-server-name").val();
-    if (serverName === "") {
-      // ../spawn/user/ causes a 404, ../spawn/user redirects correctly to the default server
-      window.location.href = "./spawn/" + user;
-    } else {
-      window.location.href = "./spawn/" + user + "/" + serverName;
+  $(".new-server-btn").click(startServer);
+  $(".new-server-name").on('keypress', function(e) {
+    if (e.which === 13) {
+      startServer.call(this);
     }
   });
 
