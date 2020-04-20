d9d23c11b340f862167a92528fe0394b04dd5153
xxshambm <ext.michael.shamberger@tieto.com>
2020-04-20 08:42:10 +0300

    use better split login to handle all ports

--- jupyterhub/apihandlers/base.py
+++ jupyterhub/apihandlers/base.py
@@ -42,11 +42,7 @@ class APIHandler(BaseHandler):
         host = self.request.headers.get("Host")
         # http spec allows for port at end of host header but this causes
         # cross site scripting check to give false positive
-        if (host.endswith(':443')):
-            host = host[:-4]
-        elif (host.endswith(':80')):
-            host = host[:-3]
-        referer = self.request.headers.get("Referer")
+        host, port = host.split(":", maxsplit=1)
 
         # If no header is provided, assume it comes from a script/curl.
         # We are only concerned with cross-site browser stuff here.
