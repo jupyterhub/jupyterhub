eaa1353dcdd17a0ebd43530e108baf197c917ae9
Min RK <benjaminrk@gmail.com>
2020-10-22 11:43:24 +0200

    typos in use of partition

--- jupyterhub/apihandlers/auth.py
+++ jupyterhub/apihandlers/auth.py
@@ -277,19 +277,23 @@ class OAuthAuthorizeHandler(OAuthHandler, BaseHandler):
         # trim protocol, which cannot be trusted with multiple layers of proxies anyway
         # Referer is set by browser, but full_url can be modified by proxy layers to appear as http
         # when it is actually https
-        referer_proto, _, stripped_referer = referer.partition("://")[2]
+        referer_proto, _, stripped_referer = referer.partition("://")
         referer_proto = referer_proto.lower()
-        req_proto, _, stripped_full_url = full_url.partition("://")[2]
+        req_proto, _, stripped_full_url = full_url.partition("://")
         req_proto = req_proto.lower()
         if referer_proto != req_proto:
             self.log.warning("Protocol mismatch: %s != %s", referer, full_url)
             if req_proto == "https":
                 # insecure origin to secure target is not allowed
-                raise web.HTTPError(403, "Not allowing authorization form submitted from insecure page")
+                raise web.HTTPError(
+                    403, "Not allowing authorization form submitted from insecure page"
+                )
         if stripped_referer != stripped_full_url:
             # OAuth post must be made to the URL it came from
             self.log.error("Original OAuth POST from %s != %s", referer, full_url)
-            self.log.error("Stripped OAuth POST from %s != %s", stripped_referer, stripped_full_url)
+            self.log.error(
+                "Stripped OAuth POST from %s != %s", stripped_referer, stripped_full_url
+            )
             raise web.HTTPError(
                 403, "Authorization form must be sent from authorization page"
             )
