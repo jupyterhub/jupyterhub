9068ff22393e561ad819e1476c9a7891e5173417
Min RK <benjaminrk@gmail.com>
2020-10-30 13:22:14 +0100

    back to dev

--- jupyterhub/_version.py
+++ jupyterhub/_version.py
@@ -4,10 +4,10 @@
 
 version_info = (
     1,
-    2,
-    1,
-    # "",  # release (b1, rc1, or "" for final or dev)
-    # "dev",  # dev or nothing for beta/rc/stable releases
+    3,
+    0,
+    "",  # release (b1, rc1, or "" for final or dev)
+    "dev",  # dev or nothing for beta/rc/stable releases
 )
 
 # pep 440 version: no dot before beta/rc, but before .dev
