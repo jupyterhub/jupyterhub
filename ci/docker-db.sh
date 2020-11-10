ebbbdcb2b1e27941b9fa92a0327d85f7bd43b192
Erik Sundell <erik.i.sundell@gmail.com>
2020-11-10 11:14:40 +0100

    Refactor ci/docker-db and ci/init-db

--- ci/docker-db.sh
+++ ci/docker-db.sh
@@ -1,59 +1,76 @@
 #!/usr/bin/env bash
-# source this file to setup postgres and mysql
-# for local testing (as similar as possible to docker)
+# This scripts starts a postgres or mysql database server, waits for it, and
+# setup setups users to be used for local testing.
 
 set -eu
 
+# These environment variables influence the mysql and psql clients
 export MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
 export MYSQL_TCP_PORT=${MYSQL_TCP_PORT:-13306}
 export PGHOST=${PGHOST:-127.0.0.1}
-NAME="hub-test-$DB"
-DOCKER_RUN="docker run -d --name $NAME"
+export PGPORT=${PGPORT:-5432}
 
-docker rm -f "$NAME" 2>/dev/null || true
+# Define a name for a database container to start
+DOCKER_CONTAINER="hub-test-$DB"
 
-case "$DB" in
-"mysql")
-  RUN_ARGS="-e MYSQL_ALLOW_EMPTY_PASSWORD=1 -p $MYSQL_TCP_PORT:3306 mysql:5.7"
-  CHECK="mysql --host $MYSQL_HOST --port $MYSQL_TCP_PORT --user root -e \q"
-  ;;
-"postgres")
-  RUN_ARGS="-e POSTGRES_HOST_AUTH_METHOD=trust -p 5432:5432 postgres:9.5"
-  CHECK="psql --user postgres -c \q"
-  ;;
-*)
-  echo '$DB must be mysql or postgres'
-  exit 1
-esac
+# Stop and remove any existing database container
+docker rm -f "$DOCKER_CONTAINER" 2>/dev/null || true
 
-$DOCKER_RUN $RUN_ARGS
+# Prepare env vars DOCKER_RUN_ARGS and READINESS_CHECK
+if [[ "$DB" == "mysql" ]]; then
+    # Environment variables are considered by the Docker image, and the mysql
+    # client respectively.
+    # ref, docker: https://hub.docker.com/_/mysql/
+    # ref, client: https://dev.mysql.com/doc/refman/5.7/en/setting-environment-variables.html
+    #
+    DOCKER_RUN_ARGS="-p $MYSQL_TCP_PORT:3306 --env MYSQL_ALLOW_EMPTY_PASSWORD=1 mysql:5.7"
+    READINESS_CHECK="mysql --user root --execute \q"
+elif [[ "$DB" == "postgres" ]]; then
+    # Environment variables are considered by the Docker image, and the psql
+    # client respectively.
+    # ref, docker: https://hub.docker.com/_/postgres/
+    # ref, client: https://www.postgresql.org/docs/9.5/libpq-envars.html
+    #
+    DOCKER_RUN_ARGS="-p $PGPORT:5432 postgres:9.5"
+    READINESS_CHECK="psql --user postgres --command \q"
+else
+    echo '$DB must be mysql or postgres'
+    exit 1
+fi
 
+# Start a new container
+docker run --detach --name "$DOCKER_CONTAINER" $DOCKER_RUN_ARGS
+
+# Wait for the new container to start
 echo -n "waiting for $DB "
 for i in {1..60}; do
-  if $CHECK; then
-    echo 'done'
-    break
-  else
-    echo -n '.'
-    sleep 1
-  fi
+    if $READINESS_CHECK; then
+        echo 'done'
+        break
+    else
+        echo -n '.'
+        sleep 1
+    fi
 done
-$CHECK
+$READINESS_CHECK
 
-case "$DB" in
-"mysql")
-  ;;
-"postgres")
-  # create the user
-  psql --user postgres -c "CREATE USER $PGUSER WITH PASSWORD '$PGPASSWORD';"
-  ;;
-*)
-esac
+# Setup additional users to mysql's root and postgresql's postgres users
+if [[ "$DB" == "mysql" ]]; then
+    echo "A dedicated user for mysql isn't created."
+elif [[ "$DB" == "postgres" ]]; then
+    psql --user postgres --command "CREATE USER $PGUSER WITH PASSWORD '$PGPASSWORD';"
+fi
 
-echo -e "
+# Print message for humans running the script
+if [[ "$DB" == "mysql" ]]; then
+    echo -e "
 Set these environment variables:
-
-    export MYSQL_HOST=127.0.0.1
+    export MYSQL_HOST=$MYSQL_HOST
     export MYSQL_TCP_PORT=$MYSQL_TCP_PORT
-    export PGHOST=127.0.0.1
 "
+elif [[ "$DB" == "postgres" ]]; then
+    echo -e "
+Set these environment variables:
+    export PGHOST=$PGHOST
+"
+fi
--- ci/init-db.sh
+++ ci/init-db.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env bash
-# initialize jupyterhub databases for testing
+# This script initialize multiple databases for testing in a running database
+# server, using either the mysql client or psql client with the root or postgres
+# users.
 
 set -eu
 
-MYSQL="mysql --user root --host ${MYSQL_HOST:-127.0.0.1} --port ${MYSQL_TCP_PORT:-13306} -e "
-PSQL="psql --user postgres -c "
+# These environment variables influence the mysql and psql clients
+export MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
+export MYSQL_TCP_PORT=${MYSQL_TCP_PORT:-13306}
+export PGHOST=${PGHOST:-127.0.0.1}
+export PGPORT=${PGPORT:-5432}
 
-case "$DB" in
-"mysql")
-  EXTRA_CREATE='CHARACTER SET utf8 COLLATE utf8_general_ci'
-  SQL="$MYSQL"
-  ;;
-"postgres")
-  SQL="$PSQL"
-  ;;
-*)
-  echo '$DB must be mysql or postgres'
-  exit 1
-esac
+# Prepare env vars CLIENT_COMMAND and EXTRA_CREATE_DATABASE_ARGS
+if [[ "$DB" == "mysql" ]]; then
+    CLIENT_COMMAND="mysql --user root --execute "
+    EXTRA_CREATE_DATABASE_ARGS='CHARACTER SET utf8 COLLATE utf8_general_ci'
+elif [[ "$DB" == "postgres" ]]; then
+    CLIENT_COMMAND="psql --user postgres --command "
+else
+    echo '$DB must be mysql or postgres'
+    exit 1
+fi
 
+# Configure a set of databases in the database server for upgrade tests
 set -x
-
 for SUFFIX in '' _upgrade_072 _upgrade_081 _upgrade_094; do
-    $SQL "DROP DATABASE jupyterhub${SUFFIX};" 2>/dev/null || true
-    $SQL "CREATE DATABASE jupyterhub${SUFFIX} ${EXTRA_CREATE:-};"
+    $CLIENT_COMMAND "DROP DATABASE jupyterhub${SUFFIX};" 2>/dev/null || true
+    $CLIENT_COMMAND "CREATE DATABASE jupyterhub${SUFFIX} ${EXTRA_CREATE_DATABASE_ARGS:-};"
 done
