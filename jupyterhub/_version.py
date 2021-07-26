1c81bda381727370c31d0541b8bad25d5a6fa365
Shubham Dhingra <82315650+shubham-quartic@users.noreply.github.com>
2021-07-26 17:14:19 +0530

    Updated version for latest pip format (#18)

--- jupyterhub/_version.py
+++ jupyterhub/_version.py
@@ -8,7 +8,7 @@
 # 0.1.0b1.dev
 # 0.1.0.dev
 
-__version__ = '1.0.0'
+__version__ = 'rc1-1.1.0'
 
 # Singleton flag to only log the major/minor mismatch warning once per mismatch combo.
 _version_mismatch_warning_logged = {}
