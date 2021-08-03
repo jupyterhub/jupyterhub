f5182fe349ad3b671fe31c03e67cde4154204b08
SHAHN3 <nsshah1288@users.noreply.github.com>
2021-08-03 17:32:34 -0400

    add explicit db rollback
    add context manager/decorator for db rollback
    
    add db rollback in top level prepare method
    
    Co-authored-by: Sarath Babu <sbreached@gmail.com>
    (cherry picked from commit 044fb23a704d2cbaa163610811afde66b61f76ac)

--- jupyterhub/handlers/base.py
+++ jupyterhub/handlers/base.py
@@ -81,6 +81,9 @@ class BaseHandler(RequestHandler):
         """
         try:
             await self.get_current_user()
+        except SQLAlchemyError:
+            self.log.exception("Rolling back session due to database error")
+            self.db.rollback()
         except Exception:
             self.log.exception("Failed to get current user")
             self._jupyterhub_user = None
@@ -423,7 +426,9 @@ class BaseHandler(RequestHandler):
                 if user and isinstance(user, User):
                     user = await self.refresh_auth(user)
                 self._jupyterhub_user = user
-            except Exception:
+            except Exception as e:
+                if isinstance(e, SQLAlchemyError):
+                    raise SQLAlchemyError()
                 # don't let errors here raise more than once
                 self._jupyterhub_user = None
                 self.log.exception("Error getting current user")
