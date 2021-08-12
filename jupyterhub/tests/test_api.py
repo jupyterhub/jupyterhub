4419f52fbaeeb02dcdb73d52a069a0236169b044
vladfreeze <vladfreeze@gmail.com>
2021-08-12 14:23:53 +0200

    Added changes for group properties editing

--- jupyterhub/tests/test_api.py
+++ jupyterhub/tests/test_api.py
@@ -1463,8 +1463,8 @@ async def test_groups_list(app):
     r.raise_for_status()
     reply = r.json()
     assert reply == [
-        {'kind': 'group', 'name': 'alphaflight', 'users': [], 'roles': []},
-        {'kind': 'group', 'name': 'betaflight', 'users': [], 'roles': []},
+        {'kind': 'group', 'name': 'alphaflight', 'users': [], 'roles': [], 'properties': {}},
+        {'kind': 'group', 'name': 'betaflight', 'users': [], 'roles': [],'properties': {}},
     ]
 
     # Test offset for pagination
@@ -1472,7 +1472,7 @@ async def test_groups_list(app):
     r.raise_for_status()
     reply = r.json()
     assert r.status_code == 200
-    assert reply == [{'kind': 'group', 'name': 'betaflight', 'users': [], 'roles': []}]
+    assert reply == [{'kind': 'group', 'name': 'betaflight', 'users': [], 'roles': [],'properties': {}}]
 
     r = await api_request(app, "groups?offset=10")
     r.raise_for_status()
@@ -1484,7 +1484,7 @@ async def test_groups_list(app):
     r.raise_for_status()
     reply = r.json()
     assert r.status_code == 200
-    assert reply == [{'kind': 'group', 'name': 'alphaflight', 'users': [], 'roles': []}]
+    assert reply == [{'kind': 'group', 'name': 'alphaflight', 'users': [], 'roles': [],'properties': {}}]
 
     r = await api_request(app, "groups?limit=0")
     r.raise_for_status()
@@ -1529,6 +1529,7 @@ async def test_group_get(app):
         'name': 'alphaflight',
         'users': ['sasquatch'],
         'roles': [],
+        'properties': {},
     }
 
 
@@ -1622,6 +1623,31 @@ async def test_group_delete_users(app):
 
     group = orm.Group.find(db, name='alphaflight')
     assert sorted([u.name for u in group.users]) == sorted(names[2:])
+
+@mark.group
+async def test_group_add_properties(app):
+    db = app.db
+    # must specify users
+    r = await api_request(app, 'groups/alphaflight/properties', method='put', data='{}')
+    assert r.status_code == 200
+
+    properties_object = {'cpu': "8", 'ram': "4", 'image':"testimage"}
+    
+    r = await api_request(
+        app,
+        'groups/alphaflight/properties',
+        method='put',
+        data=json.dumps(properties_object),
+    )
+    r.raise_for_status()
+    group = orm.Group.find(db, name='alphaflight')
+    
+    
+    assert sorted([k for k in group.properties]) == sorted([k for k in properties_object])
+    assert sorted([group.properties[k] for k in group.properties]) == sorted([properties_object[k] for k in properties_object])
+    
+    
+
 
 
 # -----------------
