89ce443d48b8cbe35b45be80e0d77de7c3e40011
vpopescu <vlad.popescu-vifor@wu.ac.at>
2021-10-14 18:33:09 +0200

    Updated code to fit with latest version

--- jsx/src/components/GroupEdit/GroupEdit.jsx
+++ jsx/src/components/GroupEdit/GroupEdit.jsx
@@ -5,17 +5,20 @@ import PropTypes from "prop-types";
 import GroupSelect from "../GroupSelect/GroupSelect";
 import DynamicTable from "../DynamicTable/DynamicTable";
 
+
 function hasDuplicates(array) {
   var valuesSoFar = Object.create(null);
   for (var i = 0; i < array.length; ++i) {
-    var value = array[i];
-    if (value in valuesSoFar) {
-      return true;
-    }
-    valuesSoFar[value] = true;
+      var value = array[i];
+      if (value in valuesSoFar) {
+          return true;
+      }
+      valuesSoFar[value] = true;
   }
   return false;
 }
+
+
 
 const GroupEdit = (props) => {
   var [selected, setSelected] = useState([]),
@@ -52,8 +55,8 @@ const GroupEdit = (props) => {
 
   var { group_data } = location.state;
   var [propobject, setProp] = useState(group_data.properties);
-  var [propkeys, setPropKeys] = useState([]);
-  var [propvalues, setPropValues] = useState([]);
+  var [propkeys, setPropKeys]= useState([]);
+  var [propvalues, setPropValues]= useState([]);
 
   if (!group_data) return <div></div>;
 
@@ -74,24 +77,19 @@ const GroupEdit = (props) => {
           setChanged(true);
         }}
       />
+      <div className="container">
       <div className="row">
-        <div className="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
           <div className="alert alert-info">Manage group properties</div>
-        </div>
       </div>
-      <div className="row">
-        <div className="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
-          <DynamicTable
-            current_propobject={group_data.properties}
-            setProp={setProp}
-            setPropKeys={setPropKeys}
-            setPropValues={setPropValues}
-            setChanged={setChanged}
-            //Add keys
-          />
-        </div>
       </div>
-
+      <DynamicTable
+        current_propobject={group_data.properties}
+        setProp={setProp}
+        setPropKeys={setPropKeys}
+        setPropValues={setPropValues}
+        setChanged ={setChanged}
+        //Add keys
+      />
       <div className="row">
         <div className="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
           <button id="return" className="btn btn-light">
@@ -121,24 +119,22 @@ const GroupEdit = (props) => {
                 promiseQueue.push(
                   removeFromGroup(removed_users, group_data.name)
                 );
-              if (hasDuplicates(propkeys) == true) {
-                error.textContent = "Duplicate key found!";
-                error.style.color = "red";
-              } else {
-                error.textContent = "";
-                propkeys.forEach((key, i) => (propobject[key] = propvalues[i]));
+              if (hasDuplicates(propkeys)==true){
+                error.textContent = "Duplicate key found!"
+                error.style.color = "red"
+              } else{
+                error.textContent = ""
+                propkeys.forEach((key, i) => propobject[key] = propvalues[i]);
               }
-              if (
-                propobject != group_data.properties &&
-                hasDuplicates(propkeys) == false
-              ) {
-                promiseQueue.push(updateProp(propobject, group_data.name));
+              if (propobject != group_data.properties && hasDuplicates(propkeys)== false) {
+                promiseQueue.push(
+                  updateProp(propobject, group_data.name)
+                );
               }
               Promise.all(promiseQueue)
                 .then(() => {
-                  updateGroups(0, limit).then((data) =>
-                    dispatchPageUpdate(data, 0)
-                  );
+                  updateGroups(0, limit)
+                    .then((data) => dispatchPageUpdate(data, 0)); 
                 })
                 .catch((err) => console.log(err));
             }}
@@ -146,8 +142,8 @@ const GroupEdit = (props) => {
             Apply
           </button>
           <div>
-            <span id="error"></span>
-          </div>
+                <span id="error"></span>
+            </div>
           <button
             id="delete-group"
             className="btn btn-danger"
