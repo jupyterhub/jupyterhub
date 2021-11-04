3caf3cfda885434bf47e652a5bf9b27d2cbedcfe
Min RK <benjaminrk@gmail.com>
2021-11-04 15:52:37 +0100

    Bump to 2.0.0rc3

--- jupyterhub/_version.py
+++ jupyterhub/_version.py
@@ -2,7 +2,7 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 # version_info updated by running `tbump`
-version_info = (2, 0, 0, "rc2", "")
+version_info = (2, 0, 0, "rc3", "")
 
 # pep 440 version: no dot before beta/rc, but before .dev
 # 0.1.0rc1
