7fe565cc0544709d3e486a09dd0bce3102701d9a
vladfreeze <vladfreeze@gmail.com>
2021-12-02 11:29:27 +0100

    updated test_scopes and access map in base.py to fit in group properties

--- jupyterhub/tests/test_scopes.py
+++ jupyterhub/tests/test_scopes.py
@@ -735,6 +735,7 @@ async def test_service_model_filtering(
                 'name',
                 'kind',
                 'users',
+                'properties',
             },
         ),
         (
