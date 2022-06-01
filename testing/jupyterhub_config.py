16870ad05f130237e294d64637e965b149b0c32e
mhasan49 <mhasan49computation@gmail.com>
2022-06-01 19:56:41 +0200

    adding admin and user accounts

--- testing/jupyterhub_config.py
+++ testing/jupyterhub_config.py
@@ -10,6 +10,13 @@ from jupyterhub.auth import DummyAuthenticator
 
 c.JupyterHub.authenticator_class = DummyAuthenticator
 
+c.JupyterHub.admin_access = True
+c.Authenticator.allowed_users = {'user1', 'user2', 'user3'}
+c.Authenticator.admin_users = {'admin1', 'admin2', 'admin3'}
+c.DummyAuthenticator.password = "admin12345"
+c.LocalAuthenticator.create_system_users = True
+c.Authenticator.delete_invalid_users = True
+
 # Optionally set a global password that all users must use
 # c.DummyAuthenticator.password = "your_password"
 
