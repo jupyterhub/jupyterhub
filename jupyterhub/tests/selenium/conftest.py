b90200667fe1b3adaa80494971e3ad38fc7d3a3c
Min RK <benjaminrk@gmail.com>
2022-09-02 13:55:48 +0200

    fix headless firefox option

--- jupyterhub/tests/selenium/conftest.py
+++ jupyterhub/tests/selenium/conftest.py
@@ -5,8 +5,8 @@ from selenium import webdriver
 @pytest.fixture()
 def browser():
     options = webdriver.FirefoxOptions()
-    options.set_headless()
-    driver = webdriver.Firefox(firefox_options=options)
+    options.headless = True
+    driver = webdriver.Firefox(options=options)
     yield driver
     driver.close()
     driver.quit()
