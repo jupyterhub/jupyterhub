cb5cc8c1b468ccab469db4107b1772460e86121e
Lili Yao <103026397+liliyao2022@users.noreply.github.com>
2022-10-26 17:58:26 +1100

    Update Groups.jsx
    "useState" was not used in this file.

--- jsx/src/components/Groups/Groups.jsx
+++ jsx/src/components/Groups/Groups.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import PropTypes from "prop-types";
 
