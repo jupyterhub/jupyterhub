eaa096152a175bc2cda34c488e13ad241939d91e
Erik Sundell <erik.i.sundell@gmail.com>
2022-11-16 10:45:51 +0100

    flake8: check F403 about import *

--- jupyterhub/apihandlers/__init__.py
+++ jupyterhub/apihandlers/__init__.py
@@ -1,5 +1,5 @@
 from . import auth, groups, hub, proxy, services, users
-from .base import *
+from .base import *  # noqa
 
 default_handlers = []
 for mod in (auth, hub, proxy, users, groups, services):
