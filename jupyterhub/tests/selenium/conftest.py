aa91a69bc8fa0438f6135eab22b5ed901a031f35
Min RK <benjaminrk@gmail.com>
2022-12-08 15:41:15 +0100

    use a single browser session for selenium tests
    avoids instantiating firefox for each test

--- jupyterhub/tests/selenium/conftest.py
+++ jupyterhub/tests/selenium/conftest.py
@@ -2,11 +2,22 @@ import pytest
 from selenium import webdriver
 
 
-@pytest.fixture()
-def browser(cleanup_after):
+@pytest.fixture(scope="session")
+def browser_session():
+    """Re-use one browser instance for the test session"""
     options = webdriver.FirefoxOptions()
     options.headless = True
     driver = webdriver.Firefox(options=options)
     yield driver
     driver.close()
     driver.quit()
+
+
+@pytest.fixture
+def browser(browser_session, cleanup_after):
+    """Get the browser session for one test
+
+    cookies are cleared after each test
+    """
+    yield browser_session
+    browser_session.delete_all_cookies()
