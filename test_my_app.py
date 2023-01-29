9279e55b15c259b73e53d813fb70fd188ce040e3
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-01-29 14:43:29 +0000

    Add playwright test

---
+++ test_my_app.py
@@ -0,0 +1,19 @@
+import re
+from playwright.sync_api import expect, Page
+
+def test_homepage_has_JupyterHub_in_title_and_Sign_in_button(page: Page):
+
+    # Open login page
+    page.goto('http://localhost:8000/hub/login')
+
+    # Expect the title to be contain the string JupyterHub
+    expect(page).to_have_title(re.compile("JupyterHub"))
+
+    # Select sign-in button using its class
+    sign_in = page.query_selector(".btn-jupyter")
+
+    # Click on sign-in button
+    sign_in.click()
+
+    # Expect the URL to contain next
+    expect(page).to_have_url(re.compile(".*next"))
