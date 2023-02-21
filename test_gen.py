c0f311d0f2b624a9f59e0fee690efcce9e5820b4
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-02-21 18:51:48 +0000

    Generate test for login page using playwright test gen feature

---
+++ test_gen.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import Page, expect
+
+
+def test_example(page: Page) -> None:
+    page.goto("http://localhost:8000/hub/login?next=%2Fhub%2F")
+    page.get_by_role("heading", name="Sign in").click()
+    page.get_by_text("Warning: JupyterHub seems to be served over an unsecured HTTP connection. We str").click()
+    page.get_by_label("Username:").click()
+    page.get_by_label("Username:").fill("hello")
+    page.get_by_label("Password:").click()
+    page.get_by_label("Password:").fill("hello")
+    page.get_by_role("button", name="Sign in").click()
