e53692d432efa42ce865add0369d3f7c8f40e89f
Min RK <benjaminrk@gmail.com>
2023-06-08 12:51:16 +0200

    https://github.com/jupyterhub/kubespawner/blob/0020c0b39e4e8c9b072f91b2875af18b6c8f57ee/kubespawner/slugs.py
    commit aa08a2d15520f9d9501d83209189afd997706b19
    Author: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Date:   Mon Feb 3 23:59:20 2025 +0000
    
        [pre-commit.ci] auto fixes from pre-commit.com hooks
    
        for more information, see https://pre-commit.ci
    
    commit eaf5cc3b5773df11dd404a6f6d5f1fd0553dcbda
    Author: Erik Sundell <erik.i.sundell@gmail.com>
    Date:   Fri Oct 18 10:29:58 2024 +0200
    
        Add escape_slug as a utility function alongside safe_slug
    
        By providing this, it becomes easier to test and compare slug changes.
    
    commit fd0a507baf09ce43b704ba99d7781921af6c13d6
    Author: Min RK <benjaminrk@gmail.com>
    Date:   Wed Oct 2 11:23:25 2024 +0200
    
        fix some safe slug patterns
    
        - escape `-` in label pattern
        - don't accept `.` in object names, to allow satisfying strict RFC 1035/1123
    
    commit dbc7682764e5538d3b0382e2de1003374f656e07
    Author: Min RK <benjaminrk@gmail.com>
    Date:   Wed Jun 26 11:07:54 2024 +0200
    
        sub '-' for any sequence of unsafe characters
    
        more recognizable with common separators like '@', '_', '.'
    
    commit 143cc8d323db5e7dba445a8c83e5925f22059e5f
    Author: Min RK <benjaminrk@gmail.com>
    Date:   Mon Jun 17 16:12:12 2024 +0200
    
        add multi_slug mechanism for multi-word slugs (username--servername---hash)
    
    commit 399b53a6ab944c1039e5a6454467d742e8724809
    Author: Min RK <benjaminrk@gmail.com>
    Date:   Mon Jun 17 11:17:33 2024 +0200
    
        let safe slug scheme live side-by-side with escape scheme
    
        - add user_server to template namespace
        - add `safe_` prefixed fields to template namespace using new 'safe' slug scheme
        - add pod_name, namespace, pvc_name to template namespace
    
    commit d6cbf20caf850e430a1cad2a6fc0ea4566f29d8f
    Author: Min RK <benjaminrk@gmail.com>
    Date:   Wed Jun 12 14:42:01 2024 +0200
    
        toward compatible transition for safe slugs
    
        - safe scheme by default
        - preserve old scheme if found
        - persist pvc_name and attempt to handle lack of persistence from before
        - add handle_legacy_names and use_legacy_labels configuration for legacy labels
    
    commit 68e00b92ffb1731e8ea0583e38e29ac6134c5a89
    Author: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Date:   Wed Jun 12 07:59:05 2024 +0000
    
        [pre-commit.ci] auto fixes from pre-commit.com hooks
    
        for more information, see https://pre-commit.ci
    
    commit 66462c5399a7a8e5c0a7a3a741c5209e546fdf1d
    Author: Min RK <benjaminrk@gmail.com>
    Date:   Thu Jun 8 12:51:16 2023 +0200
    
        add 'safe' slug scheme
    
        - always produces valid values
        - uses names, etc. as-is, when valid
        - uses stripped name--hash when needed

---
+++ jupyterhub/slugs.py
@@ -0,0 +1,212 @@
+"""Tools for generating slugs like k8s object names and labels
+
+Requirements:
+
+- always valid for arbitary strings
+- no collisions
+"""
+
+import hashlib
+import re
+import string
+
+import escapism
+
+_alphanum = tuple(string.ascii_letters + string.digits)
+_alpha_lower = tuple(string.ascii_lowercase)
+_alphanum_lower = tuple(string.ascii_lowercase + string.digits)
+_lower_plus_hyphen = _alphanum_lower + ('-',)
+
+# patterns _do not_ need to cover length or start/end conditions,
+# which are handled separately
+_object_pattern = re.compile(r'^[a-z0-9\-]+$')
+_label_pattern = re.compile(r'^[a-z0-9\.\-_]+$', flags=re.IGNORECASE)
+
+# match anything that's not lowercase alphanumeric (will be stripped, replaced with '-')
+_non_alphanum_pattern = re.compile(r'[^a-z0-9]+')
+
+# length of hash suffix
+_hash_length = 8
+
+# Make sure username and servername match the restrictions for DNS labels
+# Note: '-' is not in safe_chars, as it is being used as escape character
+_escape_slug_safe_chars = set(string.ascii_lowercase + string.digits)
+
+
+def escape_slug(name):
+    """Generate a slug with the legacy system, safe_slug is preferred."""
+    return escapism.escape(
+        name,
+        safe=_escape_slug_safe_chars,
+        escape_char='-',
+    ).lower()
+
+
+def _is_valid_general(
+    s, starts_with=None, ends_with=None, pattern=None, min_length=None, max_length=None
+):
+    """General is_valid check
+
+    Checks rules:
+    """
+    if min_length and len(s) < min_length:
+        return False
+    if max_length and len(s) > max_length:
+        return False
+    if starts_with and not s.startswith(starts_with):
+        return False
+    if ends_with and not s.endswith(ends_with):
+        return False
+    if pattern and not pattern.match(s):
+        return False
+    return True
+
+
+def is_valid_object_name(s):
+    """is_valid check for object names
+
+    Ensures all strictest object rules apply,
+    satisfying both RFC 1035 and 1123 dns label name rules
+
+    - 63 characters
+    - starts with letter, ends with letter or number
+    - only lowercalse letters, numbers, '-'
+    """
+    # object rules: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names
+    return _is_valid_general(
+        s,
+        starts_with=_alpha_lower,
+        ends_with=_alphanum_lower,
+        pattern=_object_pattern,
+        max_length=63,
+        min_length=1,
+    )
+
+
+def is_valid_label(s):
+    """is_valid check for label values"""
+    # label rules: https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/#syntax-and-character-set
+    if not s:
+        # empty strings are valid labels
+        return True
+    return _is_valid_general(
+        s,
+        starts_with=_alphanum,
+        ends_with=_alphanum,
+        pattern=_label_pattern,
+        max_length=63,
+    )
+
+
+def is_valid_default(s):
+    """Strict is_valid
+
+    Returns True if it's valid for _all_ our known uses
+
+    Currently, this is the same as is_valid_object_name,
+    which produces a valid DNS label under RFC1035 AND RFC 1123,
+    which is always also a valid label value.
+    """
+    return is_valid_object_name(s)
+
+
+def _extract_safe_name(name, max_length):
+    """Generate safe substring of a name
+
+    Guarantees:
+
+    - always starts with a lowercase letter
+    - always ends with a lowercase letter or number
+    - never more than one hyphen in a row (no '--')
+    - only contains lowercase letters, numbers, and hyphens
+    - length at least 1 ('x' if other rules strips down to empty string)
+    - max length not exceeded
+    """
+    # compute safe slug from name (don't worry about collisions, hash handles that)
+    # cast to lowercase
+    # replace any sequence of non-alphanumeric characters with a single '-'
+    safe_name = _non_alphanum_pattern.sub("-", name.lower())
+    # truncate to max_length chars, strip '-' off ends
+    safe_name = safe_name.lstrip("-")[:max_length].rstrip("-")
+    # ensure starts with lowercase letter
+    if safe_name and not safe_name.startswith(_alpha_lower):
+        safe_name = "x-" + safe_name[: max_length - 2]
+    if not safe_name:
+        # make sure it's non-empty
+        safe_name = 'x'
+    return safe_name
+
+
+def strip_and_hash(name, max_length=32):
+    """Generate an always-safe, unique string for any input
+
+    truncates name to max_length - len(hash_suffix) to fit in max_length
+    after adding hash suffix
+    """
+    name_length = max_length - (_hash_length + 3)
+    if name_length < 1:
+        raise ValueError(f"Cannot make safe names shorter than {_hash_length + 4}")
+    # quick, short hash to avoid name collisions
+    name_hash = hashlib.sha256(name.encode("utf8")).hexdigest()[:_hash_length]
+    safe_name = _extract_safe_name(name, name_length)
+    # due to stripping of '-' in _extract_safe_name,
+    # the result will always have _exactly_ '---', never '--' nor '----'
+    # use '---' to avoid colliding with `{username}--{servername}` template join
+    return f"{safe_name}---{name_hash}"
+
+
+def safe_slug(name, is_valid=is_valid_default, max_length=None):
+    """Always generate a safe slug
+
+    is_valid should be a callable that returns True if a given string follows appropriate rules,
+    and False if it does not.
+
+    Given a string, if it's already valid, use it.
+    If it's not valid, follow a safe encoding scheme that ensures:
+
+    1. validity, and
+    2. no collisions
+    """
+    if '--' in name:
+        # don't accept any names that could collide with the safe slug
+        return strip_and_hash(name, max_length=max_length or 32)
+    # allow max_length override for truncated sub-strings
+    if is_valid(name) and (max_length is None or len(name) <= max_length):
+        return name
+    else:
+        return strip_and_hash(name, max_length=max_length or 32)
+
+
+def multi_slug(names, max_length=48):
+    """multi-component slug with single hash on the end
+
+    same as strip_and_hash, but name components are joined with '--',
+    so it looks like:
+
+    {name1}--{name2}---{hash}
+
+    In order to avoid hash collisions on boundaries, use `\\xFF` as delimiter
+    """
+    hasher = hashlib.sha256()
+    hasher.update(names[0].encode("utf8"))
+    for name in names[1:]:
+        # \xFF can't occur as a start byte in UTF8
+        # so use it as a word delimiter to make sure overlapping words don't collide
+        hasher.update(b"\xff")
+        hasher.update(name.encode("utf8"))
+    hash = hasher.hexdigest()[:_hash_length]
+
+    name_slugs = []
+    available_chars = max_length - (_hash_length + 1)
+    # allocate equal space per name
+    # per_name accounts for '{name}--', so really two less
+    per_name = available_chars // len(names)
+    name_max_length = per_name - 2
+    if name_max_length < 2:
+        raise ValueError(f"Not enough characters for {len(names)} names: {max_length}")
+    for name in names:
+        name_slugs.append(_extract_safe_name(name, name_max_length))
+
+    # by joining names with '--', this cannot collide with single-hashed names,
+    # which can only contain '-' and the '---' hash delimiter once
+    return f"{'--'.join(name_slugs)}---{hash}"
