5a48a8e1fc1a1b4975f441b9b48ac42cd78c9e2b
Min RK <benjaminrk@gmail.com>
2023-06-28 15:50:48 +0200

    fix adding users to groups in tests
    not sure why this ever worked

--- jupyterhub/tests/test_scopes.py
+++ jupyterhub/tests/test_scopes.py
@@ -383,7 +383,7 @@ async def test_user_filter_with_group(app, create_user_with_scopes):
         group = orm.Group(name=group_name)
         app.db.add(group)
     for user in {user1, user2}:
-        group.users.append(user)
+        group.users.append(user.orm_user)
     app.db.commit()
 
     r = await api_request(app, 'users', headers=auth_header(app.db, user1.name))
@@ -974,7 +974,7 @@ async def test_list_users_filter(
     # create users:
     for i in (1, 2):
         user = add_user(app.db, app, name=f'in-{i}')
-        group.users.append(user)
+        group.users.append(user.orm_user)
         add_user(app.db, app, name=f'out-{i}')
     app.db.commit()
 
