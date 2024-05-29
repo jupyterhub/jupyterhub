8c3ff64511e79c2b75e14b00ce46aa1426b471a4
Simon Li <orpheus+devel@gmail.com>
2024-05-29 23:05:45 +0100

    Fix wording for `read:users` scope description

--- jupyterhub/scopes.py
+++ jupyterhub/scopes.py
@@ -64,7 +64,7 @@ scope_definitions = {
         'subscopes': ['read:users:name'],
     },
     'read:users': {
-        'description': 'Read user models (excluding including servers, tokens and authentication state).',
+        'description': 'Read user models (including servers, tokens and authentication state).',
         'subscopes': [
             'read:users:name',
             'read:users:groups',
