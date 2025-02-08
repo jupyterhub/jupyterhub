98bbae3f1889db81867fbd9ac5cbfa990d26ba7b
Simon Li <orpheus+devel@gmail.com>
2025-02-08 18:37:32 +0000

    Dsialbe pytest-playwright if installed to avoid CLI conflicts

--- docs/source/contributing/tests.md
+++ docs/source/contributing/tests.md
@@ -70,11 +70,11 @@ You can run the tests by passing the `browser` Pytest marker on the command line
 pytest -v jupyterhub/tests/browser/ -mbrowser
 ```
 
-You can set some of these [Playwright pytest command line arguments](https://playwright.dev/python/docs/test-runners#cli-arguments) to help investigate failing tests:
+You can set some of these custom Pytest command line arguments to help investigate failing tests:
 
 - `--headed` to show the automated browser whilst the tests are run
-- `--slowmo=1000` to insert a 1000 millisecond delay between each step of the test, but be aware this may hide race conditions
-- `--video=on` to save videos of tests under the `--output` directory
+- `--slowmo=1000` to insert a `1000` millisecond delay between each step of the test, but be aware this may hide race conditions
+- `--videodir=output-dir` to save videos of tests under the `output-dir` directory
 
 ## Test organisation
 
--- jupyterhub/tests/browser/conftest.py
+++ jupyterhub/tests/browser/conftest.py
@@ -5,20 +5,32 @@ from playwright.async_api import async_playwright
 
 from ..conftest import add_user, new_username
 
-# To debug failures you can pass --headed, --slowmo=<delay>, and --video=on
+
+# To debug failures you can pass these arguments on the pytest CLI
+def pytest_addoption(parser):
+    parser.addoption(
+        "--headed", action="store_true", default=False, help="Show browser"
+    )
+    parser.addoption(
+        "--slowmo",
+        action="store",
+        default=0,
+        type=int,
+        help="Delay each step by this number of milliseconds",
+    )
+    parser.addoption(
+        "--videodir",
+        action="store",
+        default=None,
+        help="Record videos of each test to this directory",
+    )
 
 
 @pytest.fixture
-def cmdoptions(request, pytestconfig):
-    # Playwright pytest supplies some CLI arguments, but they're not always taken
-    # into account so handle them ourselves
-    # https://playwright.dev/python/docs/test-runners#cli-arguments
-    print(request.__dict__)
-    print(pytestconfig.__dict__)
+def cmdoptions(request):
     return dict(
-        # (opt, request.config.getoption(f"--{opt}"))
-        (opt, pytestconfig.getoption(f"--{opt}"))
-        for opt in ["headed", "output", "slowmo", "video"]
+        (opt, request.config.getoption(f"--{opt}"))
+        for opt in ["headed", "slowmo", "videodir"]
     )
 
 
@@ -29,13 +41,10 @@ async def browser(cmdoptions):
     # browser_type in ["chromium", "firefox", "webkit"]
     async with async_playwright() as playwright:
         browser = await playwright.firefox.launch(
-            headless=not cmdoptions["headed"], slow_mo=cmdoptions["slowmo"]
+            headless=not cmdoptions["headed"],
+            slow_mo=cmdoptions["slowmo"],
         )
-        context = await browser.new_context(
-            record_video_dir=cmdoptions["output"]
-            if cmdoptions["video"] != "off"
-            else None
-        )
+        context = await browser.new_context(record_video_dir=cmdoptions["videodir"])
         page = await context.new_page()
         yield page
         await context.clear_cookies()
