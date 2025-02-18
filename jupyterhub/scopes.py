cf352f8a0d04c08913ae54f3e56cfc42b7fbc0a6
Simon Li <orpheus+devel@gmail.com>
2025-02-18 18:19:52 +0000

    Update jupyterhub/scopes.py
    Co-authored-by: Min RK <benjaminrk@gmail.com>

--- jupyterhub/scopes.py
+++ jupyterhub/scopes.py
@@ -64,7 +64,7 @@ scope_definitions = {
         'subscopes': ['read:users:name'],
     },
     'read:users': {
-        'description': 'Read user models (including state of the default server, tokens and authentication state).',
+        'description': 'Read user models (including the URL of the default server if it is running).',
         'subscopes': [
             'read:users:name',
             'read:users:groups',
