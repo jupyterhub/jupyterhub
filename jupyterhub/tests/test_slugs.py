503d3a67aa043a4ad95f109d2cc4a3a9d96144c8
Simon Li <orpheus+devel@gmail.com>
2026-04-18 00:05:38 +0100

    Update slug tests

--- jupyterhub/tests/test_slugs.py
+++ jupyterhub/tests/test_slugs.py
@@ -1,28 +1,75 @@
 import pytest
 
-from kubespawner.slugs import is_valid_label, safe_slug
+from ..slugs import (
+    is_valid_display_name,
+    is_valid_safe_slug,
+    is_valid_simple_name,
+    safe_slug,
+)
+
+
+@pytest.mark.parametrize(
+    "name, max_length, expected",
+    [
+        ("a", None, True),
+        ("a-1-2-3", None, True),
+        ("jupyter-alex-651dec0a", None, True),
+        ("1", None, False),
+        ("a--1", None, False),
+        ("-a", None, False),
+        ("a-", None, False),
+        ("üni", None, False),
+        ("a" * 30, None, True),
+        ("a" * 31, None, False),
+    ],
+)
+def test_is_valid_safe_slug(name, max_length, expected):
+    if max_length:
+        assert is_valid_safe_slug(name, max_length) == expected
+    else:
+        assert is_valid_safe_slug(name) == expected
+
+
+@pytest.mark.parametrize(
+    "name, max_length, expected",
+    [
+        ("a", None, True),
+        ("a1", None, True),
+        ("1", None, False),
+        ("a-1", None, False),
+        ("-a", None, False),
+        ("a-", None, False),
+        ("üni", None, False),
+        ("a" * 30, None, True),
+        ("a" * 31, None, False),
+    ],
+)
+def test_is_valid_simple_name(name, max_length, expected):
+    if max_length:
+        assert is_valid_simple_name(name, max_length) == expected
+    else:
+        assert is_valid_simple_name(name) == expected
 
 
 @pytest.mark.parametrize(
     "name, expected",
     [
-        ("jupyter-alex", "jupyter-alex"),
-        ("jupyter-Alex", "jupyter-alex---3a1c285c"),
-        ("jupyter-üni", "jupyter-ni---a5aaf5dd"),
-        ("endswith-", "endswith---165f1166"),
-        ("user@email.com", "user-email-com---0925f997"),
-        ("user-_@_emailß.com", "user-email-com---7e3a7efd"),
-        ("has.dot", "has-dot---03e27fdf"),
+        ("jupyter-alex", "jupyter-alex-651dec0a"),
+        ("jupyter-Alex", "jupyter-alex-3a1c285c"),
+        ("jupyter-üni", "jupyter-ni-a5aaf5dd"),
+        ("endswith-", "endswith-165f1166"),
+        ("user@email.com", "user-email-com-0925f997"),
+        ("user-_@_emailß.com", "user-email-com-7e3a7efd"),
+        ("has.dot", "has-dot-03e27fdf"),
         ("z9", "z9"),
-        ("9z9", "x-9z9---224de202"),
-        ("-start", "start---f587e2dc"),
-        ("üser", "ser---73506260"),
-        ("username--servername", "username-servername---d957f1de"),
-        ("start---f587e2dc", "start-f587e2dc---cc5bb9c9"),
-        pytest.param("x" * 63, "x" * 63, id="x63"),
-        pytest.param("x" * 64, "xxxxxxxxxxxxxxxxxxxxx---7ce10097", id="x64"),
-        pytest.param("x" * 65, "xxxxxxxxxxxxxxxxxxxxx---9537c5fd", id="x65"),
-        ("", "x---e3b0c442"),
+        ("9z9", "x-9z9-224de202"),
+        ("-start", "start-f587e2dc"),
+        ("üser", "ser-73506260"),
+        ("username-servername", "username-servername-9b109a32"),
+        ("start-f587e2dc", "start-f587e2dc-06b9709d"),
+        ("x" * 30, "x" * 30),
+        ("x" * 31, "xxxxxxxxxxxxxxxxxxxxx-0f46e4b0"),
+        ("x" * 32, "xxxxxxxxxxxxxxxxxxxxx-c62e4615"),
     ],
 )
 def test_safe_slug(name, expected):
@@ -34,9 +81,10 @@ def test_safe_slug(name, expected):
     "max_length, length, expected",
     [
         (16, 16, "x" * 16),
-        (16, 17, "xxxxx---d04fd59f"),
-        (11, 16, "error"),
-        (12, 16, "x---9c572959"),
+        (16, 17, "xxxxxxx-d04fd59f"),
+        (9, 10, "error"),
+        (12, 16, "xxx-9c572959"),
+        (30, 0, "error"),
     ],
 )
 def test_safe_slug_max_length(max_length, length, expected):
@@ -53,22 +101,21 @@ def test_safe_slug_max_length(max_length, length, expected):
 @pytest.mark.parametrize(
     "name, expected",
     [
-        ("", ""),
-        ("x", "x"),
-        ("a-b", "a-b"),
-        ("9a", "9a"),
-        ("9.", "x-9---99a1b84b"),
-        ("AbC", "AbC"),
-        ("AbC.", "abc---dbe8c5d1"),
-        ("ab.c", "ab.c"),
-        ("a@b.c", "a-b-c---d648b243"),
-        ("-x", "x---a4209624"),
-        ("x-", "x---c8b60efc"),
-        pytest.param("x" * 63, "x" * 63, id="x63"),
-        pytest.param("x" * 64, "xxxxxxxxxxxxxxxxxxxxx---7ce10097", id="x64"),
-        pytest.param("x" * 65, "xxxxxxxxxxxxxxxxxxxxx---9537c5fd", id="x65"),
+        ("eèéêë", True),
+        ("EÈÉÊË", True),
+        ("北京大学", True),
+        ("🐧 🦆 🐣 🪿", True),
+        ("∇²φ=0", True),
+        ("x x", True),
+        ("x  x", False),
+        (" x", False),
+        ("x ", False),
+        ("x\tx", False),
+        ("x\nx", False),
+        ("", False),
+        pytest.param("🐧" * 255, True, id="len255"),
+        pytest.param("🐧" * 255, True, id="len256"),
     ],
 )
-def test_safe_slug_label(name, expected):
-    slug = safe_slug(name, is_valid=is_valid_label)
-    assert slug == expected
+def test_is_valid_display_name(name, expected):
+    assert is_valid_display_name(name) == expected
