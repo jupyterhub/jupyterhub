6d9601b6b2d1dbdde2a46380463728b6297e8127
Simon Li <orpheus+devel@gmail.com>
2026-04-18 00:04:15 +0100

    WIP: thought more about slugs

--- jupyterhub/slugs.py
+++ jupyterhub/slugs.py
@@ -4,6 +4,50 @@ Requirements:
 
 - always valid for arbitary strings
 - no collisions
+
+# Design choices for safe_slug
+
+This is originally based on
+https://github.com/jupyterhub/kubespawner/blob/7.0.0/kubespawner/slugs.py
+which creates slugs for final use, e.g. object names based on username+servername
+
+safe_slug in this file is instead used for slugifying components.
+Currently it's only used to handle servernames but in future JupyterHub or a
+JupyterHub extension may use it for other components such as usernames.
+
+In general safe_slug outputs a string that is URL and DNS safe
+- Removes forbidden characters from input
+- Appends a truncated hash of the input to distinguish inputs which look the same
+  after forbidden characters are removed
+
+Ideally we want to be able to safely combine two safe_slugs in a defined way,
+e.g. if we guarantee slugs never contain '--' we can always use
+{username-slug}--{servername-slug} with no risk of clashes.
+
+If we forbid multiple consecutive `-` in the output of safe_slug we can handle
+hyphens in the input by one of:
+- a. allow `-`, always append hash
+- b. allow `-`, conditionally append hash if input contains `-` or forbidden characters
+- c. `-` is a disallowed character, always append hash
+- d. `-` is a disallowed character, conditionally append hash if input contains
+     forbidden characters
+
+E.g. depending on the choice:
+- `x-y` could end up as: `x-y-H1` or `xy-H1` where H1=HASH('x-y')
+- `xy` could end up as: `xy` or `xy-H2` where H2=HASH('xy')
+
+Note that if you require uniqueness between two different inputs, for example usernames,
+you must always call safe_slug even if the input is "safe", as it's impossible to
+distinguish between
+- `input1-input2`
+- safe_slug(`input1`) -> `input1-HASH` where `HASH` happens to equal `input2`
+
+The approach currently taken in safe_slug is (b), which has the advantage of allowing
+users to use `-` as a delimiter and still have it appear in the safe_slug.
+Forbidden characters, consecutive `-` are replaced by `-`.
+
+The default max length is 30 chars, so that two safe_slugs can be joined with `--`
+and be within the limit for a domain name label (63 chars)
 """
 
 import hashlib
@@ -11,22 +55,24 @@ import re
 import string
 import unicodedata
 
-_alphanum = tuple(string.ascii_letters + string.digits)
 _alpha_lower = tuple(string.ascii_lowercase)
 _alphanum_lower = tuple(string.ascii_lowercase + string.digits)
 
 # patterns _do not_ need to cover length or start/end conditions,
 # which are handled separately
-_object_pattern = re.compile(r'^[a-z0-9\-]+$')
-_label_pattern = re.compile(r'^[a-z0-9\.\-_]+$', flags=re.IGNORECASE)
 
 _alphanum_pattern = re.compile(r'^[a-z0-9]+$')
+# Allows `-` but not multiple consecutive `-`
+_alphanum_hyphen_pattern = re.compile(r'^[a-z0-9]+(?:-[a-z0-9]+)*$')
 
 # match anything that's not lowercase alphanumeric (will be stripped, replaced with '-')
 _non_alphanum_pattern = re.compile(r'[^a-z0-9]+')
 
 # length of hash suffix
 _hash_length = 8
+
+# default max length of slug
+_max_length = 30
 
 # Unicode printable characters excluding whitespace, based on
 # https://stackoverflow.com/questions/41757886/how-can-i-recognise-non-printable-unicode-characters-in-python/41761339#41761339
@@ -83,69 +129,44 @@ def _is_valid_general(
     return True
 
 
-def is_valid_safe_slug(s):
+def is_valid_safe_slug(s, max_length=_max_length):
     """Checks if this is a safe slug
 
-    alphanumeric, with an optional '-' in the middle
+    Requires:
+    - 1-30 characters
+    - starts with letter, ends with letter or digit
+    - only lowercase ASCII letters, digits hyphen
+    - multiple consecutive hyphens not allowed
     """
-    if len(s) > 32 or len(s) < 1:
-        return False
-    name, sep, suffix = s.partition("-")
-    if not name or (sep and not suffix):
-        return False
-    if not _alphanum_pattern.match(name):
-        return False
-    if suffix and not _alphanum_pattern.match(suffix):
-        return False
-    return True
-
-
-def is_valid_object_name(s):
-    """is_valid check for object names
-
-    Ensures all strictest object rules apply,
-    satisfying both RFC 1035 and 1123 dns label name rules
-
-    - 63 characters
-    - starts with letter, ends with letter or number
-    - only lowercalse letters, numbers, '-'
-    """
-    # object rules: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names
     return _is_valid_general(
         s,
         starts_with=_alpha_lower,
         ends_with=_alphanum_lower,
-        pattern=_object_pattern,
-        max_length=63,
+        pattern=_alphanum_hyphen_pattern,
+        max_length=max_length,
         min_length=1,
     )
 
 
-def is_valid_label(s):
-    """is_valid check for label values"""
-    # label rules: https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/#syntax-and-character-set
-    if not s:
-        # empty strings are valid labels
-        return True
+def is_valid_simple_name(s, max_length=_max_length):
+    """is this potentially untrusted string valid as slug without modification?
+
+    Requires:
+    - 1-30 characters
+    - starts with letter, ends with letter or digit
+    - only lowercase ASCII letters and digits
+
+    Hyphen `-` is not allowed here because we'll later use `-` as a substitute
+    character and a delimiter
+    """
     return _is_valid_general(
         s,
-        starts_with=_alphanum,
-        ends_with=_alphanum,
-        pattern=_label_pattern,
-        max_length=63,
+        starts_with=_alpha_lower,
+        ends_with=_alphanum_lower,
+        pattern=_alphanum_pattern,
+        max_length=max_length,
+        min_length=1,
     )
-
-
-def is_valid_default(s):
-    """Strict is_valid
-
-    Returns True if it's valid for _all_ our known uses
-
-    Currently, this is the same as is_valid_object_name,
-    which produces a valid DNS label under RFC1035 AND RFC 1123,
-    which is always also a valid label value.
-    """
-    return is_valid_object_name(s)
 
 
 def _extract_safe_name(name, max_length):
@@ -155,27 +176,27 @@ def _extract_safe_name(name, max_length):
 
     - always starts with a lowercase letter
     - always ends with a lowercase letter or number
-    - no hyphens (so clients are free to use hyphens for other purposes)
-    - only contains lowercase letters, numbers
+    - never more than one hyphen in a row (no '--')
+    - only contains lowercase letters, numbers, '-'
     - length at least 1 ('x' if other rules strips down to empty string)
     - max length not exceeded
     """
     # compute safe slug from name (don't worry about collisions, hash handles that)
     # cast to lowercase
-    # replace all non-alphanumeric characters
-    safe_name = _non_alphanum_pattern.sub("", name.lower())
-    # truncate to max_length chars
-    safe_name = safe_name[:max_length]
+    # replace any sequence of non-alphanumeric characters with a single '-'
+    safe_name = _non_alphanum_pattern.sub("-", name.lower())
+    # truncate to max_length chars, strip '-' off ends
+    safe_name = safe_name.lstrip("-")[:max_length].rstrip("-")
     # ensure starts with lowercase letter
     if safe_name and not safe_name.startswith(_alpha_lower):
-        safe_name = "x" + safe_name[: max_length - 1]
+        safe_name = "x-" + safe_name[: max_length - 1].rstrip("-")
     if not safe_name:
         # make sure it's non-empty
         safe_name = 'x'
     return safe_name
 
 
-def strip_and_hash(name, max_length=32):
+def strip_and_hash(name, max_length=_max_length):
     """Generate an always-safe, unique string for any input
 
     truncates name to max_length - len(hash_suffix) to fit in max_length
@@ -187,67 +208,37 @@ def strip_and_hash(name, max_length=32):
     # quick, short hash to avoid name collisions
     name_hash = hashlib.sha256(name.encode("utf8")).hexdigest()[:_hash_length]
     safe_name = _extract_safe_name(name, name_length)
-    # the result will always have _exactly_ one '-'
     return f"{safe_name}-{name_hash}"
 
 
-def safe_slug(name, is_valid=is_valid_default, max_length=32):
+def safe_slug(name, is_valid=is_valid_simple_name, max_length=_max_length):
     """Always generate a safe slug for a non-empty string
 
-    is_valid should be a callable that returns True if a given string follows appropriate rules,
-    and False if it does not.
+    The returned slug will:
+    - match regex [a-z0-9-]{1,max_length} (default max_length is 30)
+    - be unique for a given input (within the probabilty of an 8 hex-digit hash collision)
+    - no consecutive hypens (`--`)
+    - starts with [a-z]
+    - ends with [a-z0-9]
+    - if the input matches [a-z][a-z0-9]{0,max_length-1} (i.e. only contains lowercase
+      ASCII letters and digits, no hyphens, starts with a lowercase letter)
+      it is returned unmodified
+    - in all other cases it is modified to only contain [a-z0-9-] with no consecutive hyphens,
+      and additionally is appended with a `-` and a 8 hex-digit hash
 
-    Given a string, if it's already valid, use it.
-    If it's not valid, follow a safe encoding scheme that ensures:
-
-    1. validity, and
-    2. no collisions
+    This ensures that:
+    - concatenating any number of safe_slugs with `--` will always be unique
+    - concatenating two safe_slugs with `--` will be a valid DNS name
+      (RFC 1035 and 1123: ASCII letters, digits, hyphens, max length 63)
+    - it is always possible to tell whether a safe_slug required changing the input:
+      if it was modified it will always end in `-HASH` where `HASH` is determined by
+      JupyterHub, not the input e.g. `user-suffix` will become `user-suffix-HASH`
     """
     if not name:
         raise ValueError("Unable to create safe slug for empty string")
-    if '-' in name:
-        # don't accept any names that could collide with the safe slug
-        return strip_and_hash(name, max_length=max_length)
-    # allow max_length override for truncated sub-strings
-    if is_valid(name) and (max_length is None or len(name) <= max_length):
+    if is_valid(name, max_length):
         return name
-    else:
-        return strip_and_hash(name, max_length=max_length)
-
-
-def multi_slug(names, max_length=48):
-    """multi-component slug with single hash on the end
-
-    same as strip_and_hash, but name components are joined with '--',
-    so it looks like:
-
-    {name1}--{name2}---{hash}
-
-    In order to avoid hash collisions on boundaries, use `\\xFF` as delimiter
-    """
-    hasher = hashlib.sha256()
-    hasher.update(names[0].encode("utf8"))
-    for name in names[1:]:
-        # \xFF can't occur as a start byte in UTF8
-        # so use it as a word delimiter to make sure overlapping words don't collide
-        hasher.update(b"\xff")
-        hasher.update(name.encode("utf8"))
-    hash = hasher.hexdigest()[:_hash_length]
-
-    name_slugs = []
-    available_chars = max_length - (_hash_length + 1)
-    # allocate equal space per name
-    # per_name accounts for '{name}--', so really two less
-    per_name = available_chars // len(names)
-    name_max_length = per_name - 2
-    if name_max_length < 2:
-        raise ValueError(f"Not enough characters for {len(names)} names: {max_length}")
-    for name in names:
-        name_slugs.append(_extract_safe_name(name, name_max_length))
-
-    # by joining names with '--', this cannot collide with single-hashed names,
-    # which can only contain '-' and the '---' hash delimiter once
-    return f"{'--'.join(name_slugs)}---{hash}"
+    return strip_and_hash(name, max_length)
 
 
 def is_valid_display_name(s):
@@ -255,6 +246,7 @@ def is_valid_display_name(s):
 
     Between 1 and 255 chars
     Can contain most printable non whitespace unicode characters
+    Can contain space (0x20)
     Must not start or end with space
     Must not contain multiple consecutive spaces
     """
