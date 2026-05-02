215a52fcbda9fdadef8c70aa623dafcec3d27945
Carol Willing <carolcode@willingconsulting.com>
2026-05-01 18:41:15 -0700

    rebase cruft

--- jupyterhub/tests/browser/conftest.py
+++ jupyterhub/tests/browser/conftest.py
@@ -11,24 +11,6 @@ async def _browser():
     # browser_type in ["chromium", "firefox", "webkit"]
     async with async_playwright() as playwright:
         browser = await playwright.firefox.launch(headless=True)
-<<<<<<< HEAD
-        context = await browser.new_context()
-        # context sets default timeout for a lot of things, but not expect
-        context.set_default_timeout(30_000)
-        # default timeout for expect
-        expect.set_options(timeout=30_000)
-        page = await context.new_page()
-        yield page
-        await context.clear_cookies()
-        await browser.close()
-||||||| parent of a24e5300 (Split browser into 2 logical entities)
-        context = await browser.new_context()
-        context.set_default_timeout(30_000)
-        page = await context.new_page()
-        yield page
-        await context.clear_cookies()
-        await browser.close()
-=======
         yield browser
 
 
@@ -42,20 +24,7 @@ async def browser(_browser):
     page = await context.new_page()
     yield page
     await context.clear_cookies()
-<<<<<<< HEAD
-<<<<<<< HEAD
     await context.close()
->>>>>>> a24e5300 (Split browser into 2 logical entities)
-||||||| parent of f1f82b3c (increase default expect timeout to 30s)
-    await context.close()
-=======
-    await browser.close()
->>>>>>> f1f82b3c (increase default expect timeout to 30s)
-||||||| parent of f4747d64 (update close)
-    await browser.close()
-=======
-    await context.close()
->>>>>>> f4747d64 (update close)
 
 
 @pytest.fixture
