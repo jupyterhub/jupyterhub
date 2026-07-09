548c384c067bf5b7099b074acfbc7f2985fe4106
Dawa Ometto <d.l.a.ometto1@uu.nl>
2026-07-09 10:08:07 +0200

    Test if CI are failures are due to a race condition

--- jupyterhub/proxy.py
+++ jupyterhub/proxy.py
@@ -654,7 +654,7 @@ class ConfigurableHTTPProxy(Proxy):
                     os.kill(pid, sig_list[i])
             except ProcessLookupError:
                 break
-            time.sleep(1)
+            time.sleep(4)
             try:
                 self._check_pid(pid)
             except ProcessLookupError:
